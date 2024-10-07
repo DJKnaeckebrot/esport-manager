e63eb2fea53e2d6aab8903894958d48c282e813f
DJKnaeckebrot <dennis.lueke@pm.me>
2024-10-07 12:56:13 +0200

    feat: darkmode

--- app/(dashboard)/dashboard/layout.tsx
+++ app/(dashboard)/dashboard/layout.tsx
@@ -36,7 +36,7 @@ export default function DashboardLayout({
   return (
     <div className="flex flex-col min-h-[calc(100dvh-68px)] max-w-7xl mx-auto w-full">
       {/* Mobile header */}
-      <div className="lg:hidden flex items-center justify-between bg-white border-b border-gray-200 p-4">
+      <div className="lg:hidden flex items-center justify-between border-b border-gray-200 dark:border-gray-700 p-4">
         <div className="flex items-center">
           <span className="font-medium">Settings</span>
         </div>
@@ -53,7 +53,7 @@ export default function DashboardLayout({
       <div className="flex flex-1 overflow-hidden h-full">
         {/* Sidebar */}
         <aside
-          className={`w-64 bg-white lg:bg-gray-50 border-r border-gray-200 lg:block ${
+          className={`w-64 lg:bg-gray-50 dark:lg:bg-gray-900 border-r border-gray-200 dark:border-gray-700 lg:block ${
             isSidebarOpen ? "block" : "hidden"
           } lg:relative absolute inset-y-0 left-0 z-40 transform transition-transform duration-300 ease-in-out lg:translate-x-0 ${
             isSidebarOpen ? "translate-x-0" : "-translate-x-full"
@@ -65,7 +65,7 @@ export default function DashboardLayout({
                 <Button
                   variant={pathname === item.href ? "secondary" : "ghost"}
                   className={`my-1 w-full justify-start ${
-                    pathname === item.href ? "bg-gray-100" : ""
+                    pathname === item.href ? "bg-gray-100 dark:bg-gray-700" : ""
                   }`}
                   onClick={() => setIsSidebarOpen(false)}
                 >
