ac7685be72f9ce47973301e0aef2593c81ffb46d
DJKnaeckebrot <dennis.lueke@pm.me>
2024-10-14 10:39:48 +0200

    fix: add missing import

--- components/member/add-member-modal.tsx
+++ components/member/add-member-modal.tsx
@@ -2,7 +2,7 @@
 
 import { Button } from "@/components/ui/button";
 import { addOrgMember } from "@/app/(dashboard)/actions";
-import { useActionState, useState } from "react";
+import { useActionState, useEffect, useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Label } from "../ui/label";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
