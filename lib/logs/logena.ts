9c4aba9118b5c3aab9994a0371af3db8412c08f4
DJKnaeckebrot <dennis.lueke@pm.me>
2024-10-16 15:41:00 +0200

    feat: add logena

---
+++ lib/logs/logena.ts
@@ -0,0 +1,15 @@
+import Logena from "logena";
+
+Logena.set({
+  debug: true,
+  appName: "E-Sports Manager",
+  colors: {
+    appName: "brightRed",
+    levels: {
+      info: "green",
+    },
+  },
+  useTimestamps: true,
+});
+
+export default Logena;
